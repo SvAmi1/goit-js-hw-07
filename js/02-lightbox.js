e9008c44665a6f4eddb0928739f8b9096c5a20f5
SvAmi1 <svmoshko877@gmail.com>
2023-06-12 12:43:34 +0300

    ex 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createGalleryMarkup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" 
+   href=${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+    }
+    
+    const galleryEl = document.querySelector(".gallery");
+    const imgMarkup = createGalleryMarkup (galleryItems);
+    
+    galleryEl.insertAdjacentHTML("afterbegin", imgMarkup);
+    
+    
+   var lightbox = new SimpleLightbox(".gallery a",
+        {captionsData: "alt",
+        captionDelay: 250,});
+
+
+// console.log(galleryItems);
